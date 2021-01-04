c555531949cc036a90d10e1b087b3c5e59a5fde3
agvantibo <derez08@yandex.ru>
2021-01-04 14:21:00 +0300

    Refactored C to fit pep8

--- c.py
+++ c.py
@@ -13,38 +13,39 @@ AvailableCities = []
 NodeScan = [2 for i in range(n)]
 NodeScan[0] = 1
 
-def Crawl(Node, Gas):
+
+def crawl(node, gas):
     # (0) Checking if node has been scanned twice. If it is, exit cleanly.
-    if NodeScan[Node - 1] <= 0:
+    if NodeScan[node - 1] <= 0:
         return 0
     else:
-        NodeScan[Node - 1] -= 1
+        NodeScan[node - 1] -= 1
     # (1) Refill gas tank if possible
-    if Node in GasStations:
-        Gas = s
+    if node in GasStations:
+        gas = s
     # (2) Compile a list of connections for nodes
-    Connections = []
+    connections = []
     for i in Roads:
-        if i[0] == Node:
-            Connections.append([i[1], i[2]])
-        elif i[1] == Node:
-            Connections.append([i[0], i[2]])
+        if i[0] == node:
+            connections.append([i[1], i[2]])
+        elif i[1] == node:
+            connections.append([i[0], i[2]])
     # (3) Check if it is possible to travel to a node.
-    AvailableConnections = []
-    for i in Connections:
-        if i[1] <= Gas:
-            AvailableConnections.append(i)
-    del Connections
-    # (3.5) Flush AvailableConnections to AvailableCities
-    for i in AvailableConnections:
+    available_connections = []
+    for i in connections:
+        if i[1] <= gas:
+            available_connections.append(i)
+    del connections
+    # (3.5) Flush available_connections to AvailableCities
+    for i in available_connections:
         if i[0] not in AvailableCities:
             AvailableCities.append(i[0])
     # (4) ~Recluse!~ Recurse!
-    for i in AvailableConnections:
-        Crawl(i[0], Gas - i[1])
+    for i in available_connections:
+        crawl(i[0], gas - i[1])
 
 
-Crawl(1, k)
+crawl(1, k)
 print(len(AvailableCities))
 AvailableCities.sort()
 print(" ".join(map(str, AvailableCities)))
