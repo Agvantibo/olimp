e3bed0111ccb0a5541ad2ef28f0d460082ed1b56
agvantibo <derez08@yandex.ru>
2021-01-04 19:44:53 +0300

    Tried rewriting B (main) with a more sophisticated algorithm. - It stores what teachers have been already used - It checks whether the teacher was used for this job in the past. So it should never assign one teacher 2 different works, and vice versa. Debugging commits should follow.

--- b.py
+++ b.py
@@ -6,27 +6,33 @@ n, m, k = [int(i) for i in input().split()]
 
 Teachers = [i + 1 for i in range(m)]
 JobStatus = [k for i in range(n)]
-hours = ceil(k * n / m)
-Jobs = [[0 for j in range(n)] for i in range(hours)]
-tc = -2
+Hours = ceil(k * n / m)
+Jobs = [[0 for j in range(n)] for i in range(Hours)]
+LockedTeachers = []
 
 
-def get_teacher(cur, length):
-    return Teachers[cur % length]
+def write_teacher(i1, i2):
+    for a in range(m):
+        if LockedTeachers[a] == 0:
+            if i == 0:
+                LockedTeachers[a] = 1
+                Jobs[i1][i2] = Teachers[a]
+            elif Teachers[a] == Jobs[i1-1][i2]:
+                continue
 
 
-for i in range(hours):
-    tc += 1
+for i in range(Hours):
     ut = 0
+    LockedTeachers = [0 for f in range(m)]
     for j in range(n):
-        if JobStatus[j] != 0:
-            tc += 1
-            ut += 1
-            JobStatus[j] -= 1
-            Jobs[i][j] = get_teacher(tc, m)
+        if JobStatus[j] > 0:
             if ut == m:
                 break
+            else:
+                ut += 1
+                write_teacher(i, j)
 
-print(hours)
+
+print(Hours)
 for i in Jobs:
     print(" ".join(map(str, i)))
