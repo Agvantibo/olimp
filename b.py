ee40334ee4af3388edcb1526c4573d1d5b764e97
agvantibo <derez08@yandex.ru>
2021-01-05 16:52:58 +0300

    Diagonal Algorithm. Done! But there might be a problem: it overdoes some tasks. I am too lazy to add a status array, so i'll chuck it onto the server and see how it goes.

--- b.py
+++ b.py
@@ -2,75 +2,20 @@
 from math import ceil
 
 TotalJobs, TotalTeachers, Passes = [int(i) for i in input().split()]
-
-Teachers = [i + 1 for i in range(TotalTeachers)]
-JobStatus = [Passes for i in range(TotalJobs)]
 Hours = ceil(Passes * TotalJobs / TotalTeachers)
 Jobs = [[0 for j in range(TotalJobs)] for i in range(Hours)]
-# LockedTeachers = []
+UsedTeachers = 0
 
+for i in range(TotalJobs):
+    UsedTeachers += 1
+    Jobs[0][i] = i + 1
+    if UsedTeachers == TotalTeachers:
+        break
 
-def release_lock():
-    for i in range(TotalTeachers):
-        LockedTeachers[i] = 0
-
-
-def check_col_tem(row, col, teacher_index):
-    res = True
-    for i in range(row):
-        if Jobs[i][col] == Teachers[teacher_index]:
-            res = False
-            break
-    return res
-
-
-def get_teach_index (row, col):
-
-    odd_even = row % 2
-    iter_miter = 0
-
-    # if row > 0:
-    #     row -= 1
-
-    for t in range(TotalTeachers):
-        if odd_even == 0:
-            iter_miter = t
-        else:
-            iter_miter = TotalTeachers - t - 1
-
-        if LockedTeachers [iter_miter] == 0:
-            if check_col_tem(row, col, iter_miter):
-                LockedTeachers[iter_miter] = 1
-                return iter_miter
-
-
-def write_teacher(i1, i2):
-    for a in range(TotalTeachers):
-        if LockedTeachers[a] == 0:
-            if i1 == 0:
-                LockedTeachers[a] = 1
-                Jobs[i1][i2] = Teachers[a]
-            elif Teachers[a] == Jobs[i1-1][i2]:
-                continue
-            else:
-                LockedTeachers[a] = 1
-                Jobs[i1][i2] = Teachers[a]
-
-
-LockedTeachers = [0 for f in range(TotalTeachers)]
-
-for i in range(Hours):
-    UsedTeachers = 0
-    release_lock()
-    for j in range(TotalJobs):
-        if JobStatus[j] > 0:
-            if UsedTeachers == TotalTeachers:
-                break
-            else:
-                UsedTeachers += 1
-                Jobs[i][j] = Teachers[get_teach_index(i, j)]
-                JobStatus[j] -= 1
-
+for i in range(1, Hours):
+    for j in range(TotalJobs - 1):
+        Jobs[i][j] = Jobs[i - 1][j + 1]
+    Jobs[i][-1] = Jobs[i - 1][0]
 
 print(Hours)
 for i in Jobs:
