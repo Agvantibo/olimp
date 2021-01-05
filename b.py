6e6f42d5293ad83f81ea6d6467c907891ff63433
agvantibo <derez08@yandex.ru>
2021-01-05 17:22:07 +0300

    Added a nonzero check. Should be better.

--- b.py
+++ b.py
@@ -2,32 +2,35 @@
 from math import ceil
 
 TotalJobs, TotalTeachers, Passes = [int(i) for i in input().split()]
-Hours = ceil(Passes * TotalJobs / TotalTeachers)
-Jobs = [[0 for j in range(TotalJobs)] for i in range(Hours)]
-JobStatus = [Passes for i in range(TotalJobs)]
-UsedTeachers = 0
+if TotalJobs == 0 or Passes == 0:
+    print(0)
+else:
+    Hours = ceil(Passes * TotalJobs / TotalTeachers)
+    Jobs = [[0 for j in range(TotalJobs)] for i in range(Hours)]
+    JobStatus = [Passes for i in range(TotalJobs)]
+    UsedTeachers = 0
 
 
-def check_job(row, column, value):
-    if JobStatus[column] > 0 and value != 0:
-        JobStatus[column] -= 1
-        Jobs[row][column] = value
-        return 0
-    else:
-        return 1
+    def check_job(row, column, value):
+        if JobStatus[column] > 0 and value != 0:
+            JobStatus[column] -= 1
+            Jobs[row][column] = value
+            return 0
+        else:
+            return 1
 
 
-for i in range(TotalJobs):
-    UsedTeachers += 1
-    check_job(0, i, i + 1)
-    if UsedTeachers == TotalTeachers:
-        break
+    for i in range(TotalJobs):
+        UsedTeachers += 1
+        check_job(0, i, i + 1)
+        if UsedTeachers == TotalTeachers:
+            break
 
-for i in range(1, Hours):
-    for j in range(TotalJobs - 1):
-        check_job(i, j, Jobs[i - 1][j + 1])
-    check_job(i, -1, Jobs[i - 1][0])
+    for i in range(1, Hours):
+        for j in range(TotalJobs - 1):
+            check_job(i, j, Jobs[i - 1][j + 1])
+        check_job(i, -1, Jobs[i - 1][0])
 
-print(Hours)
-for i in Jobs:
-    print(" ".join(map(str, i)))
+    print(Hours)
+    for i in Jobs:
+        print(" ".join(map(str, i)))
