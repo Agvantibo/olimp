44a689f2237ff8cef97e2ebe9e5844e4f6c96396
agvantibo <derez08@yandex.ru>
2021-01-05 17:11:08 +0300

    Added the check status array. Probably should fix everything.

--- b.py
+++ b.py
@@ -4,18 +4,29 @@ from math import ceil
 TotalJobs, TotalTeachers, Passes = [int(i) for i in input().split()]
 Hours = ceil(Passes * TotalJobs / TotalTeachers)
 Jobs = [[0 for j in range(TotalJobs)] for i in range(Hours)]
+JobStatus = [Passes for i in range(TotalJobs)]
 UsedTeachers = 0
+
+
+def check_job(row, column, value):
+    if JobStatus[column] > 0 and value != 0:
+        JobStatus[column] -= 1
+        Jobs[row][column] = value
+        return 0
+    else:
+        return 1
+
 
 for i in range(TotalJobs):
     UsedTeachers += 1
-    Jobs[0][i] = i + 1
+    check_job(0, i, i + 1)
     if UsedTeachers == TotalTeachers:
         break
 
 for i in range(1, Hours):
     for j in range(TotalJobs - 1):
-        Jobs[i][j] = Jobs[i - 1][j + 1]
-    Jobs[i][-1] = Jobs[i - 1][0]
+        check_job(i, j, Jobs[i - 1][j + 1])
+    check_job(i, -1, Jobs[i - 1][0])
 
 print(Hours)
 for i in Jobs:
