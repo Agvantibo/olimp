899cc5863514f14f7e094240e18c8657f7e51099
agvantibo <derez08@yandex.ru>
2021-01-18 20:37:10 +0300

    Fixed C. It was terrible, with features missing. Now it's better...

--- Moscow Olimp 2021/C.py
+++ Moscow Olimp 2021/C.py
@@ -1,23 +1,28 @@
 #!/bin/python3
+from sys import exit
 tests = int(input())
-test_scores = []
-for i in range(tests):
-    test_scores.append(int(input()))
+test_scores = [int(i) for i in input().split()]
 final_score = int(input())
 n_submissions = int(input())
 
-submissions = [[] for i in range(n_submissions)]
-for i in submissions:
-    for j in range(tests):
-        if int(input()) == 1:
-            i.append(True)
-        else:
-            i.append(False)
+submissions = [[int(i) for i in input().split()] for i in range(n_submissions)]
 
+penalty = 0
+MaxScore = 0
 for i in range(n_submissions):
+    FullTests = True
     score = 0
-    penalty = i * 2
     for j in range(tests):
-        if submissions[i][j]:
+        if submissions[i][j] == 1:
             score += test_scores[j]
-    print(score-penalty)
+        elif submissions[i][j] == 0:
+            FullTests = False
+        else:
+            exit("Press F to pay respects")
+    score -= penalty
+    if FullTests:
+        score += final_score
+    if score > MaxScore:
+        MaxScore = score
+    print(MaxScore)
+    penalty += 2
