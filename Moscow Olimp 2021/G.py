9acaf71368ccd1f43a33b5096aedb72cdf1f84ba
agvantibo <derez08@yandex.ru>
2021-02-14 16:33:11 +0300

    SOLVED G! At least for the first tests

--- Moscow Olimp 2021/G.py
+++ Moscow Olimp 2021/G.py
@@ -1,4 +1,5 @@
 #!/bin/pypy3
+from math import ceil
 parties, N_bulletins = [int(i) for i in input().split()]
 bulletins = []
 
@@ -17,11 +18,16 @@ del bulletins
 
 total = 0
 for i in partyVotes:
-    total += 1
+    total += i
 
 for i in range(parties):
-    partyVotes[i] = partyVotes[i] + 1 // total - 1
+    partyVotes[i] = ceil(partyVotes[i] / (total / 100))
 
+to_print = ''
 for i in range(parties):
-    if partyVotes[i] > 17:
-        print(i + 1)
+    if partyVotes[i] >= 7:
+        to_print += str(i + 1) + " "
+
+del parties
+del partyVotes
+print(to_print)
