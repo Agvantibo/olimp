390fba18520e6b467f893ea146537bf0a5d7f231
agvantibo <derez08@yandex.ru>
2021-02-19 12:16:04 +0300

    ???

--- Letovo/Search/A-minified.py
+++ Letovo/Search/A-minified.py
@@ -1,11 +1,25 @@
 #!/bin/pypy3
 
+
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+        else:
+            return binary_search(arr, mid + 1, high, x)
+    else:
+        return -1
+
+
 input()
 data = [int(i) for i in input().split()]
 queries = [int(i) for i in input().split()]
 
 for i in queries:
-    if i in data:
-        print("YES")
-    else:
+    if binary_search(data, 0, len(data) - 1, i) == -1:
         print("NO")
+    else:
+        print("YES")
